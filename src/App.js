8607522d80f58985d325f79034cbf3efb9bf8531
Ansvyager <Ansvyager@gmail.com>
2021-10-02 02:02:48 +0700

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import Expenses from "./components/Expenses/Expenses";
+const App = () => {
+  const expenses = [
+    {
+      id: 1,
+      title: "Car insurance",
+      amount: 294.76,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 2,
+      title: "Car Inova",
+      amount: 594.76,
+      date: new Date(2021, 3, 28),
+    },
+    {
+      id: 3,
+      title: "Car Yamaha",
+      amount: 194.76,
+      date: new Date(2021, 4, 21),
+    },
+    {
+      id: 4,
+      title: "Car Honda",
+      amount: 494.76,
+      date: new Date(2021, 5, 22),
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2 style={{ color: "#ffff" }}>Andres Car Shop</h2>
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
